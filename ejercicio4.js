3213948d469a0140912a1cf099c1bb31dc6e7661
AlanParrillo <alan.parrillo@tecsup.edu.pe>
2023-09-21 19:00:51 -0500

    Laboratorio3 terminado

---
+++ ejercicio4.js
@@ -0,0 +1,17 @@
+var nombre = 'Juan';
+var apellido = 'Perez';
+
+var NombreCompleto = nombre + ' ' + apellido;
+console.log(NombreCompleto);
+
+var NombreCompleto2 = 'Carlos' + ' ' + 'Lara';
+console.log(NombreCompleto2);
+
+var x = nombre + 2 + 4;
+console.log(x);
+
+x = nombre + (2 + 4);
+console.log(x);
+
+x = 2 + 4 + nombre;
+console.log(x);
---
+++ ejercicio6.js
@@ -0,0 +1,10 @@
+let NombreCompleto = "Juan Parez";
+let Nombrecompleto = "Carlos lara";
+console.log( NombreCompleto );
+console.log( Nombrecompleto );
+
+let alNombreCompleto;
+let _NombreCompleto;
+let $NombreCompleto;
+
+let = ruptura = 10;
